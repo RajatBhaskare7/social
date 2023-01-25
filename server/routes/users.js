8f005381d11459d4d1ffcfa090e103e3d0501adc
Rajat Bhaskare <80106274+RajatBhaskare7@users.noreply.github.com>
2023-01-25 23:11:22 +0530

    name change

--- server/routes/users.js
+++ server/routes/users.js
@@ -11,7 +11,6 @@ const router = express.Router();
 /* READ */
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
-
 /* UPDATE */
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
